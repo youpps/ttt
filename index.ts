12ebe93df4a07209be92c5604638420fd4b3e351
youpps <riashenka@gmail.com>
2023-04-18 22:10:27 +0300

    das

--- index.ts
+++ index.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
 
     await client.getMe();
 
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox"] });
     const page = await browser.newPage();
 
     await page.setViewport({
